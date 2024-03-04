c03affdda573c353cfda3f4d7f773f99288feee4
Quinn <quinn@indra.ai>
2024-03-04 14:15:20 -0800

    WebDeva Update main code set

--- index.js
+++ index.js
@@ -21,14 +21,10 @@ const info = {
 const {agent,vars} = require('./data.json').DATA;
 const Deva = require('@indra.ai/deva');
 const WEB = new Deva({
-  agent: {
-    uid: agent.uid,
-    key: agent.key,
-    name: agent.name,
-    describe: agent.describe,
-    prompt: agent.prompt,
-    voice: agent.voice,
-    profile: agent.profile,
+  info,
+  agent,
+  vars,
+  utils: {
     translate(input) {
       return input.trim().replace(/[$|#|@]/g, '');
     },
@@ -39,14 +35,18 @@ const WEB = new Deva({
                 .replace(/\'/g, '&#39;')
                 .replace(/\`/g, '&#96;')
       return input.trim();
+    },
+    process(input) {
+      // input = input.replace(/<script>.*?<\/script>/gms, '')
+      //              .replace(/<style(.+)?>.*?<\/style>/gms, '');
+      // console.log('INPUT', input);
+      return input;
     }
   },
-  vars,
   listeners: {},
   modules: {
     xmlparser: false,
   },
-  deva: {},
   func: {
     /**************
     func: post
@@ -72,29 +72,13 @@ const WEB = new Deva({
     describe: get a url response.
     ***************/
     get(url) {
+      this.action('func', 'get');
       return new Promise((resolve, reject) => {
         if (!url) return reject('NO URL');
         axios.get(url, {
           headers: this.vars.headers
         }).then(result => {
-          const rObject = typeof result.data === 'object' && result.data !== null ? true : false;
-          const rArray = Array.isArray(result.data);
-
-          let text, html, data = {};
-          const htmldata = result.data.toLowerCase().includes('<body');
-          if (htmldata) {
-            const body = result.data;
-            const _$ = cheerio.load(body, null, false);
-            text = _$.text();
-            html = _$.html();
-            data = {};
-          }
-          else {
-            text = result.data;
-            html = `<pre><code>${this.agent.parse(result.data)}</code></pre>`;
-          }
-
-          return resolve({text,html,data});
+          return resolve(this.utils.process(result.data));
         }).catch(err => {
           console.log('web error', err)
           return this.error(err);
@@ -135,53 +119,49 @@ const WEB = new Deva({
     describe: Return a rss feed
     ***************/
     rss(url) {
+      this.action('func', `rss:${url}`);
+      const agent = this.agent();
+
       return new Promise((resolve, reject) => {
         let data, text;
-        if (!url) return reject('NO URL');
+        this.state('get', url);
         axios.get(url, {
           headers: this.vars.headers
         }).then(result => {
           try {
             const {channel} = this.modules.xmlparser.parse(result.data).rss;
             const buildrss = [
-              `# ${channel.title}`,
-              `p: ${channel.description}`,
-              `published: ${channel.lastBuildDate}`,
-              `link[${this.agent.translate(channel.title)}]:${channel.link}`,
+              `## ${channel.title}`,
+              `describe: ${channel.description}`,
+              `date: ${channel.lastBuildDate}`,
+              `link[${this.utils.translate(channel.title)}]:${channel.link}`,
               `copyright: ${channel.copyright}`,
               `\n-\n`,
             ];
-            channel.item.forEach(itm => {
+            channel.item.forEach((itm,idx) => {
               let $desc = itm.description ? cheerio.load(itm.description, null, false) : false;
-              $desc = $desc.text();
               const item = [
                 `::begin:rssitem`,
-                `## ${itm.title.replace(/\n/g, '')}`,
-                $desc ? `p: ${this.lib.trimText($desc, 300)}` : '',
-                itm.link ? `link[${this.agent.translate(itm.title)}]:${itm.link}` : '',
-                !itm.link && itm.guid ? `link[${this.agent.translate(itm.title)}]:${itm.guid}` : '',
+                `### ${itm.title.replace(/\n/g, '')}`,
+                $desc ? `describe: ${this.trimWords($desc.text(), 75)}` : '',
                 itm.pubDate ? `date: ${itm.pubDate}` : '',
+                itm.link ? `button[Article Link]:${this.askChr}web get ${itm.link}` : '',
+                !itm.link && itm.guid ? `button[Article Guid]:${this.askChr}web get ${itm.guid}` : '',
                 `::end:rssitem`,
-              ].join('\n\n');
-              buildrss.push(item);
+                '\n---\n',
+              ].join('\n');
+              if (idx < this.vars.rss.max_records) buildrss.push(item);
             });
-            text = buildrss.join('\n\n');
+            text = buildrss.join('\n');
             data = channel;
           } catch (e) {
-            return this.error(e, url, reject);
+            this.state('reject', url);
+            return reject(e);
           } finally {
-            return this.question(`#feecting parse:${this.agent.key} ${text}`);
+            this.state('resolve', url);
+            return resolve({text,data});
           }
-        }).then(parsed => {
-          return resolve({
-            text:parsed.a.text,
-            html:parsed.a.html,
-            data
-          });
-        }).catch(err => {
-          console.log('web error', err)
-          return this.error(err);
-        })
+        }).catch(reject)
       });
     }
   },
@@ -190,32 +170,50 @@ const WEB = new Deva({
       return this.func.post(packet.q);
     },
     get(packet) {
-      return this.func.get(packet.q.text);
+      return new Promise((resolve, reject) => {
+        this.func.get(packet.q.text).then(result => {
+          this.state('resolve', 'get');
+          return resolve({
+            text: true,
+            html: true,
+            data: true,
+          })
+        }).catch(err => {
+          this.state('reject', 'get');
+          return this.error(err, packet, reject);
+        })
+      });
     },
     json(packet) {
       return this.func.json(packet.q.text);
     },
     rss(packet) {
-      return this.func.rss(packet.q.text);
-    },
-    uid(packet) {
-      return Promise.resolve(this.uid());
-    },
-    status(packet) {
-      return this.status();
-    },
-    help(packet) {
       return new Promise((resolve, reject) => {
-        this.lib.help(packet.q.text, __dirname).then(help => {
-          return this.question(`#feecting parse ${help}`);
+        if (!packet) return resolve(this._messages.nopacket);
+        if (!packet.q.text) return resolce(this._messages.notext);
+        if (packet.q.meta.params[1]) this.vars.rss.max_records = packet.q.meta.params[1];
+        const data = {};
+
+        this.func.rss(packet.q.text).then(feed => {
+          data.feed = feed.data;
+          this.state('parse', packet.q.text);
+          return this.question(`${this.askChr}feecting parse ${feed.text}`);
+
         }).then(parsed => {
+          data.feecting = parsed.a.data;
+          this.state('resolve', packet.q.text);
+
           return resolve({
             text: parsed.a.text,
             html: parsed.a.html,
-            data: parsed.a.data,
+            data,
           });
-        }).catch(reject);
+        }).catch(err => {
+          this.state('reject', packet.q.text);
+          return this.error(err, packet, reject);
+        })
       });
+      return this.func.rss(packet.q);
     },
   },
   onError(err) {
